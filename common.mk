8047c12c1e9ca6031bdb9633864135a5826d50f5
Yillié <yilliee@protonmail.com>
2022-04-03 19:10:51 +0500

    common : Include aosp_base.mk, languages_full.mk; Include common.mk from pbrp vendor instead of twrp

--- common.mk
+++ common.mk
@@ -1,8 +1,9 @@
 # Inherit from the common Open Source product configuration
-$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
 # Inherit from our custom product configuration
-$(call inherit-product, vendor/twrp/config/common.mk)
+$(call inherit-product, vendor/pb/config/common.mk)
 
 # Dynamic Partition Flag
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
